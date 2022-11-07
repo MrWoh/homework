b3176cdcdc3679a42195462e754b85734bde3813
Paulius Verseckas <103728574+MrWoh@users.noreply.github.com>
2022-11-07 18:34:59 +0200

    added move after get monthly

--- ba_homework/core/get_monthly.py
+++ ba_homework/core/get_monthly.py
@@ -1,3 +1,4 @@
+import shutil
 import datetime as dt
 from calendar import monthrange
 import pandas as pd
@@ -8,12 +9,12 @@ unc_data = os.path.join(UNC_FILE_FOLDER, '')
 
 # find all days data
 for file in os.listdir(unc_data):
+    df = pd.read_parquet(os.path.join(unc_data, file))
+    date = df.loc[0:0, 'tpep_pickup_datetime'].values.astype(str)
+    date = date[0].split('-')
+    year, month = int(date[0]), int(date[1])
     if file.endswith('.parquet'):
-        df = pd.read_parquet(os.path.join(unc_data, file))
         if file.endswith('.parquet'):
-            date = df.loc[0:0, 'tpep_pickup_datetime'].values.astype(str)
-            date = date[0].split('-')
-            year, month = int(date[0]), int(date[1])
             num_days = monthrange(year, month)
             for day in range(2, num_days[1]+1):
                 df = df[df.tpep_dropoff_datetime.dt.date ==
@@ -21,3 +22,14 @@ for file in os.listdir(unc_data):
                 file_name = os.path.join(
                     unc_data, 'yellow_tripdata_{}-{}-{}.parquet'.format(int(year), int(month), day))
                 df.to_parquet(file_name, engine='fastparquet')
+    c_data_new_folder = 'yellow_tripdata_{}-{}'.format(int(year), int(month))
+    c_data_move_dest = os.path.join(C_FILE_FOLDER, c_data_new_folder, '')
+    os.mkdir(c_data_move_dest)
+    # fetch all files
+    for file_name in os.listdir(unc_data):
+        # construct full file path
+        source = unc_data + file_name
+        destination = c_data_move_dest + file_name
+        # move only files
+        if os.path.isfile(source):
+            shutil.move(source, destination)
